fdc72cfd4c8cfc846e7c73cd1f3fa8c5153b3b2d
oloartedev <32349951+omar5919@users.noreply.github.com>
2021-03-05 20:01:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var app = express();
 
+console.log('prueba');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
